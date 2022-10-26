d096b22eabfb91f2e43d2dccf6eb4a8a4e4bee64
Jorge Cruz <jcruz42@ucol.mx>
2022-10-26 00:21:43 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,55 @@
+import Producto from "./producto.js";
+import Inventario from "./inventario.js";
+
+let inventario = new Inventario();
+
+const btnAgregar = document.getElementById("agregar");
+btnAgregar.addEventListener('click', () => {
+    let tcod = document.getElementById("codigo").value;
+    let tnom = document.getElementById("nombre").value;
+    let tcan = document.getElementById("cantidad").value;
+    let tcos = document.getElementById("costo").value;
+
+    let pro = new Producto(tnom, tcod, tcan, tcos);
+    inventario.agregar(pro);
+    listado.innerHTML += `Se ha agregado un producto al inventario: ${tcod}.- Nombre: ${tnom} - Cantidad: ${tcan} - Costo: ${tcos}<br>`;
+});
+
+const btnEliminar = document.getElementById('eliminar');
+btnEliminar.addEventListener('click', ()=>{
+    let tcod = document.getElementById("codigo").value;
+
+    inventario.eliminar(tcod);
+    listado.innerHTML += `Se ha eliminado un producto ${tcod} del inventario <br>`;
+})
+
+const btnListar = document.getElementById("listar");
+btnListar.addEventListener('click', ()=>{
+    let tabla = document.getElementById("tabla");
+    tabla.innerHTML = '';
+    tabla.innerHTML = inventario.listado();
+    document.getElementById('titulolistado').innerHTML='';
+    document.getElementById('titulolistado').innerHTML='Listado';
+    listado.innerHTML += "Se enlistaron los productos del inventario <br>"
+});
+
+const btnListarInvertido = document.getElementById("listarInverso");
+btnListarInvertido.addEventListener('click', ()=>{
+    let tabla = document.getElementById("tabla");
+    tabla.innerHTML = '';
+    tabla.innerHTML = inventario.listadoInverso();
+    document.getElementById('titulolistado').innerHTML='';
+    document.getElementById('titulolistado').innerHTML='Listado Inverso';
+    listado.innerHTML += "Se enlistaron los productos del inventario a la inversa <br>"
+});
+
+
+const btnBuscar = document.getElementById("buscar");
+btnBuscar.addEventListener("click",()=>{
+    let tcod = document.getElementById("codigo").value; 
+    let pro = inventario.busqueda(tcod);
+
+    document.getElementById("listadoTotal").innerHTML += pro;
+    listado.innerHTML += "Se realizo la busqueda del producto <br>"
+     
+});
---
+++ inventario.js
@@ -0,0 +1,100 @@
+export default class Inventario{
+    constructor(){
+        this.primero = null;
+        this.size = 0;
+    }
+
+
+    agregar(nuevo){
+        if(this.primero===null){
+            this.primero = nuevo;
+        }else{
+            let aux = this.primero;
+            while(aux.next != null){
+                aux = aux.next;
+            }
+            aux.next = nuevo;
+        }
+        this.size++
+    }
+
+    agregarInicio(nuevo){
+        primero = this.primero;
+
+        nuevo.next = primero; 
+    }
+
+    eliminar(numero){
+        let current = this.primero;
+        let previous = null;
+
+        while(current != null){
+            if(current.numero === numero){
+                if(!previous){
+                    this.primero = current.next;
+                }else {
+                    previous.next = current.next;
+                }
+                this.size--;
+                return current.numero;
+            }
+            else{
+                previous=current;
+                current=current.next;
+            }
+        }
+        return null;
+    }
+
+
+    listado(){
+        if(!this.size){
+            return null;
+        } else {
+            let current = this.primero;
+            let result = '';
+            while (current!=null) {
+                result += `<tr id='${current.codigo}'><td>Codigo: ${current.codigo} Nombre: ${current.nombre} Cantidad: ${current.cantidad} Costo: ${current.costo} </td></tr>`;
+                current = current.next;
+            }
+    
+            return result;
+        }
+    }
+
+    listadoInverso(){
+        if(!this.size){
+            return null;
+        } else {
+            let current = this.primero;
+            let result = '';
+            
+            while (current) {
+                result = `<tr id='${current.codigo}'><td>Codigo: ${current.codigo} Nombre: ${current.nombre} Cantidad: ${current.cantidad} Costo: ${current.costo}</td></tr>`+result;
+                current = current.next;
+            }
+    
+            return result;
+        }
+    }
+
+
+    busqueda(codigo){
+        let current = this.primero;
+        let previous = null;
+        let result = "";
+        let key = false;
+
+        while(current != null && key != true){
+            if(current.codigo == codigo){
+                key = true
+                result += `<tr id='${current.codigo}'><td>Codigo: ${current.codigo} Nombre: ${current.nombre} Cantidad: ${current.cantidad} Costo: ${current.costo} </td></tr>`;
+            }
+            else{
+                previous=current;
+                current=current.next;
+            }
+        }
+        return result;
+    }
+}
